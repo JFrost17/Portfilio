33dd92ff4c5e2dbbe227ac0a698a9a0cdab297db
JFrost17 <jmcleod17@live.ca>
2025-05-02 09:19:50 -0400

    add app domain in text for test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TextField, Grid } from '@mui/material'
 
 function App() {
 
   return (
     <>
       <div>
-       
+       <Grid container item xs={12}>
+          <TextField> https://jackimdb.netlify.app/ </TextField>
+       </Grid>
       </div>
     </>
   )
